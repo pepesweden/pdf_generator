eb1f407db35f9cfd8b44229830490c5c70b590b8
pepesweden <warme.patrick@gmail.com>
2025-04-30 17:22:00 +0200

    skapat pdf-skript och uppdaterat req...txt

---
+++ backend/pdf_service.py
@@ -0,0 +1,23 @@
+from fpdf import FPDF
+import io
+
+def generate_pdf(text: str) -> bytes:
+    print("Text som skickas in:", text) #Kontrollerar att text variavlen innegåller något"
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.multi_cell(0, 10, text)
+
+    # Spara som bytes i minnet
+    output = io.BytesIO()
+    pdf_string = pdf.output(dest='S').encode('latin1')
+    print("📄 PDF-genererad, antal bytes:", len(pdf_string))  # <- kontroll av längden på strängen in i PDF
+    return pdf_string
+
+
+# Testkörning
+if __name__ == "__main__":
+    pdf_string = generate_pdf("Hej Patrick! Detta är din första PDF via script.")
+    with open("test.pdf", "wb") as f:
+        f.write(pdf_string)
+    print("✅ PDF sparad som test.pdf")

c941178093b6d8396b37091715f506f55e48590e
pepesweden <warme.patrick@gmail.com>
2025-04-30 23:36:37 +0200

    Skapat flask scirpt fil i app.py

---
+++ pdf_service/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, Response
+from pdf_service import generate_pdf  # Vi använder din funktion från tidigare
+
+app = Flask(__name__)
+
+@app.route("/generate_pdf", methods=["POST"])
+def handle_generate_pdf():
+    data = request.get_json()
+    text = data.get("text", "")
+
+    print("📥 Text mottagen:", text)
+    pdf_bytes = generate_pdf(text)
+
+    return Response(pdf_bytes, mimetype="application/pdf")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
